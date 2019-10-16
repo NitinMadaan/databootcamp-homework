9d50929fb96fed56ae0533dfcf3bcbf0b08ff60c
Nitin Madaan <nitin.madaan22@gmail.com>
2019-10-16 14:41:51 -0400

    Changed name

--- Unit3-Python/Homework_Python_Elections.py
+++ Unit3-Python/Homework_Python_Elections.py
@@ -1,76 +1,107 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[42]:
+# In[28]:
 
 
 import os
 import csv
-import decimal
 
-budget_csv = "/Users/nitinmadaan/Desktop/UOFT/Resources/budget_data.csv"
+election_csv = "/Users/nitinmadaan/Desktop/UOFT/Resources/election_data.csv"
 
-change_list={}
+TV=0
+cc=0
+Candidates = set()
 
-TR=0
-total_sum=0.00
-row_amount = 0.00
-p_ra=0.00
-change=0.00
-p_change = 0.00
+c_list=[]
 
 
-with open(budget_csv, 'r') as csvfile:
+# In[29]:
+
+
+with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
     csv_header = next(csvreader)
     print(f"CSV Header:{csv_header}")
     for row in csvreader:
-        TR=TR+1
-        row_amount = float(row[1])
-        ra = float(row_amount)
-        total_sum = total_sum + row_amount
-        if (p_ra != 0.00):
-            change = ra - p_ra
-            
-        p_ra = ra
+        TV=TV+1
+        Candidates.add(row[2])
         
-        c_change = change + p_change
-        
-        p_change = c_change
-        
-        change_list.update({row[0]:change})
-        
-       
-    print("Financial Analysis")
-    print("----------------------------") 
-    print("Total Months:",TR)
-    print("Total: $",total_sum)
-    print("Total change: $",c_change/(TR-1))
-    print("Greatest Increase in Profits:",max(zip(change_list.values(), change_list.keys())))
-    print("Greatest Decrease in Profits:",min(zip(change_list.values(), change_list.keys()))) 
+    print("Election Results")
+    print("-------------------------")
+    print("Total Votes:" ,TV)
+    print("-------------------------")
+    
+    c_list =list(Candidates)
+    
+i = 0
 
 
-# In[44]:
+length = len(c_list)
+vote_count = [0] * length 
 
 
-with open('filename', 'w') as f:
-    print("Financial Analysis", file=f)
+with open(election_csv, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader: 
+    
+    
+        for i in range(length):
+            if row[2] == c_list[i]:
+                vote_count[i] += 1
+
+
+p = 0       
+p_votes = []
+      
+
+for p in range(length):
+    pv = round(vote_count[p] / TV * 100.00, 2)
+    p_votes.append(pv)
+
+
+max_votes = max(vote_count)
+
+max_index = vote_count.index(max_votes)
+
+election_winner = c_list[max_index]
+         
+
+
+# In[31]:
+
+
+with open('election_results', 'w') as f:
+    print("ELECTION RESULTS", file=f)
     print("----------------------------",file=f) 
-    print("Total Months:",TR,file=f)
-    print("Total: $",total_sum,file=f)
-    print("Total change: $",c_change/(TR-1),file=f)
-    print("Greatest Increase in Profits:",max(zip(change_list.values(), change_list.keys())),file=f)
-    print("Greatest Decrease in Profits:",min(zip(change_list.values(), change_list.keys())),file=f)
+    for (x, y, z) in zip(c_list, p_votes, vote_count):
+        print("Name: ", x ,"; Percent: ", y, "; Total Votes: ", z ,file=f)
+    print("---------------------------------------------------",file=f)
+    print("Total votes: " + str(TV),file=f)
+    print("---------------------------------------------------",file=f)
+    print("Winner: " + str(election_winner),file=f)
 
 
-# In[22]:
+# In[ ]:
 
 
 
 
 
-# In[34]:
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 

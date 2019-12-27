f22c34d1bf6c81e0b684874c38266a2e105f71e4
Nitin Madaan <nitin.madaan22@gmail.com>
2019-12-27 14:06:11 -0500

    HWUpdate

---
+++ Unit10-Web Scraping/app.py
@@ -0,0 +1,34 @@
+
+
+# Dependencies and Setup
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+
+# Flask Setup
+
+app = Flask(__name__)
+
+
+# PyMongo Connection 
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
+
+# Define Main Behavior
+if __name__ == "__main__":
+    app.run()
